27c365f1a9b6558c7f4ebe337f3fb0fd17bae798
Anders Jenbo <anders@jenbo.dk>
2019-07-21 05:41:05 +0200

    Update render code

--- Source/render.cpp
+++ Source/render.cpp
@@ -2,15 +2,19 @@
 
 DEVILUTION_BEGIN_NAMESPACE
 
-int WorldBoolFlag = 0;
-unsigned int gdwCurrentMask = 0;
-// char world_4B3264 = 0;
-unsigned char *gpCelFrame = NULL;
-unsigned int *gpDrawMask = NULL;
-BYTE *gpDrawTbl = NULL;
-// char world_4B326D[16] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+#define NO_OVERDRAW
+#define USE_SPEEDCELS
 
-unsigned int RightMask[32] = {
+typedef enum {
+	RT_SQUARE,
+	RT_TRANSPARENT,
+	RT_LTRIANGLE,
+	RT_RTRIANGLE,
+	RT_LTRAPEZOID,
+	RT_RTRAPEZOID
+};
+
+static DWORD RightMask[32] = {
 	0xE0000000, 0xF0000000,
 	0xFE000000, 0xFF000000,
 	0xFFE00000, 0xFFF00000,
@@ -29,7 +33,7 @@ unsigned int RightMask[32] = {
 	0xFFFFFFFF, 0xFFFFFFFF
 };
 
-unsigned int LeftMask[32] = {
+static DWORD LeftMask[32] = {
 	0x00000003, 0x0000000F,
 	0x0000003F, 0x000000FF,
 	0x000003FF, 0x00000FFF,
@@ -48,7 +52,7 @@ unsigned int LeftMask[32] = {
 	0xFFFFFFFF, 0xFFFFFFFF
 };
 
-unsigned int WallMask[32] = {
+static DWORD WallMask[32] = {
 	0x00000000, 0x00000000,
 	0x00000000, 0x00000000,
 	0x00000000, 0x00000000,
@@ -67,201 +71,223 @@ unsigned int WallMask[32] = {
 	0x00000000, 0x00000000
 };
 
-int WorldTbl3x16[48] = {
-	0, 0, 0,
-	4, 4, 4,
-	8, 8, 8,
-	12, 12, 12,
-	16, 16, 16,
-	20, 20, 20,
-	24, 24, 24,
-	28, 28, 28,
-	32, 32, 32,
-	36, 36, 36,
-	40, 40, 40,
-	44, 44, 44,
-	48, 48, 48,
-	52, 52, 52,
-	56, 56, 56,
-	60, 60, 60
+static DWORD SolidMask[32] = {
+	0xFFFFFFFF, 0xFFFFFFFF,
+	0xFFFFFFFF, 0xFFFFFFFF,
+	0xFFFFFFFF, 0xFFFFFFFF,
+	0xFFFFFFFF, 0xFFFFFFFF,
+	0xFFFFFFFF, 0xFFFFFFFF,
+	0xFFFFFFFF, 0xFFFFFFFF,
+	0xFFFFFFFF, 0xFFFFFFFF,
+	0xFFFFFFFF, 0xFFFFFFFF,
+	0xFFFFFFFF, 0xFFFFFFFF,
+	0xFFFFFFFF, 0xFFFFFFFF,
+	0xFFFFFFFF, 0xFFFFFFFF,
+	0xFFFFFFFF, 0xFFFFFFFF,
+	0xFFFFFFFF, 0xFFFFFFFF,
+	0xFFFFFFFF, 0xFFFFFFFF,
+	0xFFFFFFFF, 0xFFFFFFFF,
+	0xFFFFFFFF, 0xFFFFFFFF
 };
 
-// slope/angle tables, left and right
-int WorldTbl17_1[17] = { 0, 4, 8, 16, 24, 36, 48, 64, 80, 100, 120, 144, 168, 196, 224, 256, 288 };
-int WorldTbl17_2[17] = { 0, 32, 60, 88, 112, 136, 156, 176, 192, 208, 220, 232, 240, 248, 252, 256, 288 };
-
-/*
- 32x32 arch types
- add 8 if light index is 0
-
-	|-| 0x8 (0)
-	|-|
-
-	/\  0x9 (1)
-	\/
-
-	 /| 0xA (2)
-	 \|
-
-	|\  0xB (3)
-	|/
-
-	|-| 0xC (4)
-	 \|
-
-	|-| 0xD (5)
-	|/
-*/
-
-__inline void CopyLine(BYTE **dst, BYTE **src, int w)
+inline static void RenderLine(BYTE **dst, BYTE **src, int n, BYTE *tbl, DWORD mask)
 {
 	int i;
-	unsigned int mask;
 
-	if (gpDrawMask != NULL) {
-		mask = *gpDrawMask;
-		gpDrawMask--;
+#ifdef NO_OVERDRAW
+	if (zoomflag) {
+		if ((*dst) < &gpBuffer[(0 + 160) * BUFFER_WIDTH]
+		    || (*dst) > &gpBuffer[(352 + 160) * BUFFER_WIDTH]) {
+			(*src) += n;
+			(*dst) += n;
+			return;
+		}
+	} else {
+		if ((*dst) < &gpBuffer[(-17 + 160) * BUFFER_WIDTH]
+		    || (*dst) > &gpBuffer[(160 + 160) * BUFFER_WIDTH]) {
+			(*src) += n;
+			(*dst) += n;
+			return;
+		}
 	}
-	for (i = 0; i < w; i++, (*src)++, (*dst)++, mask <<= 1) {
-		if (gpDrawMask != NULL && !(mask & 0x80000000)) {
-			if (light_table_index == lightmax) {
-				(*dst)[0] = pTransTbl[0][(*dst)[0]];
-			} else if (gpDrawTbl) {
-				(*dst)[0] = pTransTbl[gpDrawTbl[(*src)[0]]][(*dst)[0]];
-			} else {
-				(*dst)[0] = pTransTbl[(*src)[0]][(*dst)[0]];
+#endif
+
+	if (mask == 0xFFFFFFFF) {
+		if (light_table_index == lightmax) {
+			(*src) += n;
+			for (i = 0; i < n; i++, (*dst)++) {
+				(*dst)[0] = 0;
+			}
+#ifdef USE_SPEEDCELS
+		} else if (tbl == NULL) {
+#else
+		} else if (light_table_index == 0) {
+#endif
+			for (i = n & 3; i != 0; i--, (*src)++, (*dst)++) {
+				(*dst)[0] = (*src)[0];
+			}
+			for (i = n >> 2; i != 0; i--, (*src) += 4, (*dst) += 4) {
+				((DWORD *)(*dst))[0] = ((DWORD *)(*src))[0];
 			}
 		} else {
-			if (light_table_index == lightmax) {
-				(*dst)[0] = 0;
-			} else if (gpDrawTbl) {
-				(*dst)[0] = gpDrawTbl[(*src)[0]];
-			} else {
-				(*dst)[0] = (*src)[0];
+			for (i = 0; i < n; i++, (*src)++, (*dst)++) {
+				(*dst)[0] = tbl[(*src)[0]];
+			}
+		}
+	} else {
+		if (light_table_index == lightmax) {
+			(*src) += n;
+			for (i = 0; i < n; i++, (*dst)++, mask <<= 1) {
+				if (mask & 0x80000000) {
+					(*dst)[0] = 0;
+				} else {
+					(*dst)[0] = pTransTbl[0][(*dst)[0]];
+				}
+			}
+#ifdef USE_SPEEDCELS
+		} else if (tbl == NULL) {
+#else
+		} else if (light_table_index == 0) {
+#endif
+			for (i = 0; i < n; i++, (*src)++, (*dst)++, mask <<= 1) {
+				if (mask & 0x80000000) {
+					(*dst)[0] = (*src)[0];
+				} else {
+					(*dst)[0] = pTransTbl[(*src)[0]][(*dst)[0]];
+				}
+			}
+		} else {
+			for (i = 0; i < n; i++, (*src)++, (*dst)++, mask <<= 1) {
+				if (mask & 0x80000000) {
+					(*dst)[0] = tbl[(*src)[0]];
+				} else {
+					(*dst)[0] = pTransTbl[tbl[(*src)[0]]][(*dst)[0]];
+				}
 			}
 		}
 	}
 }
 
-void drawUpperScreen(BYTE *pBuff)
+void RenderTile(BYTE *pBuff)
 {
 	int i, j;
-	BYTE width;
+	char c, v, tile;
 	BYTE *src, *dst, *tbl;
-	short cel_type_16;
-	DWORD *pFrameTable;
+	DWORD m, *mask, *pFrameTable;
 
-	gpDrawMask = NULL;
-	gpDrawTbl = NULL;
-
-	if (cel_transparency_active) {
-		if (arch_draw_type == 0) {
-			gpDrawMask = &WallMask[31];
-		} else if (arch_draw_type == 1) {
-			if (block_lvid[level_piece_id] == 1 || block_lvid[level_piece_id] == 3) {
-				gpDrawMask = &LeftMask[31];
-			}
-		} else if (arch_draw_type == 2) {
-			if (block_lvid[level_piece_id] == 2 || block_lvid[level_piece_id] == 3) {
-				gpDrawMask = &RightMask[31];
-			}
-		}
-	}
-
-	gpCelFrame = (unsigned char *)SpeedFrameTbl;
 	dst = pBuff;
 	pFrameTable = (DWORD *)pDungeonCels;
 
-	if (light_table_index == lightmax) {
-		if (level_cel_block & 0x8000)
-			level_cel_block = *(DWORD *)&gpCelFrame[64 * (level_cel_block & 0xFFF)] + (level_cel_block & 0xF000);
-		src = &pDungeonCels[pFrameTable[level_cel_block & 0xFFF]];
-		cel_type_16 = level_cel_block >> 12;
-	} else if (!light_table_index) {
-		if (level_cel_block & 0x8000)
-			level_cel_block = *(DWORD *)&gpCelFrame[64 * (level_cel_block & 0xFFF)] + (level_cel_block & 0xF000);
-		src = &pDungeonCels[pFrameTable[level_cel_block & 0xFFF]];
-		cel_type_16 = level_cel_block >> 12;
-	} else if (!(level_cel_block & 0x8000)) {
-		src = &pDungeonCels[pFrameTable[level_cel_block & 0xFFF]];
-		gpDrawTbl = (BYTE *)&pLightTbl[256 * light_table_index];
-		cel_type_16 = level_cel_block >> 12;
-	} else {
-		src = &pSpeedCels[*(DWORD *)&gpCelFrame[4 * (light_table_index + 16 * (level_cel_block & 0xFFF))]];
-		cel_type_16 = (level_cel_block >> 12) - 8;
-	}
+	src = &pDungeonCels[pFrameTable[level_cel_block & 0xFFF]];
+	tile = (level_cel_block & 0x7000) >> 12;
+	tbl = &pLightTbl[256 * light_table_index];
 
-	switch (cel_type_16 & 7) {
-	case 0:
-		for (i = 32; i; i--, dst -= BUFFER_WIDTH + 32) {
-			CopyLine(&dst, &src, 32);
+#ifdef USE_SPEEDCELS
+	if (light_table_index == lightmax || light_table_index == 0) {
+		if (level_cel_block & 0x8000) {
+			level_cel_block = SpeedFrameTbl[level_cel_block & 0xFFF][0] + (level_cel_block & 0xF000);
+		}
+		src = &pDungeonCels[pFrameTable[level_cel_block & 0xFFF]];
+		tile = (level_cel_block & 0x7000) >> 12;
+		tbl = NULL;
+	} else if (level_cel_block & 0x8000) {
+		src = &pSpeedCels[SpeedFrameTbl[level_cel_block & 0xFFF][light_table_index]];
+		tile = (level_cel_block & 0x7000) >> 12;
+		tbl = NULL;
+	}
+#endif
+
+	mask = &SolidMask[31];
+
+	if (cel_transparency_active) {
+		if (arch_draw_type == 0) {
+			mask = &WallMask[31];
+		}
+		if (arch_draw_type == 1 && tile != RT_LTRIANGLE) {
+			c = block_lvid[level_piece_id];
+			if (c == 1 || c == 3) {
+				mask = &LeftMask[31];
+			}
+		}
+		if (arch_draw_type == 2 && tile != RT_RTRIANGLE) {
+			c = block_lvid[level_piece_id];
+			if (c == 2 || c == 3) {
+				mask = &RightMask[31];
+			}
+		}
+	}
+/*
+#ifdef _DEBUG
+	if (GetAsyncKeyState(VK_MENU) & 0x8000) {
+		mask = &SolidMask[31];
+	}
+#endif
+*/
+	switch (tile) {
+	case RT_SQUARE:
+		for (i = 32; i != 0; i--, dst -= BUFFER_WIDTH + 32, mask--) {
+			RenderLine(&dst, &src, 32, tbl, *mask);
 		}
 		break;
-	case 1:
-		for (i = 32; i; i--, dst -= BUFFER_WIDTH + 32) {
-			for (j = 32; j;) {
-				width = *src++;
-				if (!(width & 0x80)) {
-					j -= width;
-					CopyLine(&dst, &src, width);
+	case RT_TRANSPARENT:
+		for (i = 32; i != 0; i--, dst -= BUFFER_WIDTH + 32, mask--) {
+			m = *mask;
+			for (j = 32; j != 0; j -= v, m <<= v) {
+				v = *src++;
+				if (v >= 0) {
+					RenderLine(&dst, &src, v, tbl, m);
 				} else {
-					width = -(char)width;
-					dst += width;
-					j -= width;
+					v = -v;
+					dst += v;
 				}
 			}
 		}
 		break;
-	case 2:
-		for (i = 30; i >= 0; i -= 2, dst -= BUFFER_WIDTH + 32) {
-			dst += i;
+	case RT_LTRIANGLE:
+		for (i = 30; i >= 0; i -= 2, dst -= BUFFER_WIDTH + 32, mask--) {
 			src += i & 2;
-			CopyLine(&dst, &src, 32 - i);
+			dst += i;
+			RenderLine(&dst, &src, 32 - i, tbl, *mask);
 		}
-		for (i = 2; i != 32; i += 2, dst -= BUFFER_WIDTH + 32) {
-			dst += i;
+		for (i = 2; i != 32; i += 2, dst -= BUFFER_WIDTH + 32, mask--) {
 			src += i & 2;
-			CopyLine(&dst, &src, 32 - i);
+			dst += i;
+			RenderLine(&dst, &src, 32 - i, tbl, *mask);
 		}
 		break;
-	case 3:
-		for (i = 30; i >= 0; i -= 2, dst -= BUFFER_WIDTH + 32) {
-			CopyLine(&dst, &src, 32 - i);
+	case RT_RTRIANGLE:
+		for (i = 30; i >= 0; i -= 2, dst -= BUFFER_WIDTH + 32, mask--) {
+			RenderLine(&dst, &src, 32 - i, tbl, *mask);
 			src += i & 2;
 			dst += i;
 		}
-		for (i = 2; i != 32; i += 2, dst -= BUFFER_WIDTH + 32) {
-			CopyLine(&dst, &src, 32 - i);
+		for (i = 2; i != 32; i += 2, dst -= BUFFER_WIDTH + 32, mask--) {
+			RenderLine(&dst, &src, 32 - i, tbl, *mask);
 			src += i & 2;
 			dst += i;
 		}
 		break;
-	case 4:
-		for (i = 30; i >= 0; i -= 2, dst -= BUFFER_WIDTH + 32) {
-			dst += i;
+	case RT_LTRAPEZOID:
+		for (i = 30; i >= 0; i -= 2, dst -= BUFFER_WIDTH + 32, mask--) {
 			src += i & 2;
-			CopyLine(&dst, &src, 32 - i);
+			dst += i;
+			RenderLine(&dst, &src, 32 - i, tbl, *mask);
 		}
-		for (i = 16; i; i--, dst -= BUFFER_WIDTH + 32) {
-			CopyLine(&dst, &src, 32);
+		for (i = 16; i != 0; i--, dst -= BUFFER_WIDTH + 32, mask--) {
+			RenderLine(&dst, &src, 32, tbl, *mask);
 		}
 		break;
-	default:
-		for (i = 30; i >= 0; i -= 2, dst -= BUFFER_WIDTH + 32) {
-			CopyLine(&dst, &src, 32 - i);
+	case RT_RTRAPEZOID:
+		for (i = 30; i >= 0; i -= 2, dst -= BUFFER_WIDTH + 32, mask--) {
+			RenderLine(&dst, &src, 32 - i, tbl, *mask);
 			src += i & 2;
 			dst += i;
 		}
-		for (i = 16; i; i--, dst -= BUFFER_WIDTH + 32) {
-			CopyLine(&dst, &src, 32);
+		for (i = 16; i != 0; i--, dst -= BUFFER_WIDTH + 32, mask--) {
+			RenderLine(&dst, &src, 32, tbl, *mask);
 		}
 		break;
 	}
-}
-
-void drawLowerScreen(BYTE *pBuff)
-{
-	drawUpperScreen(pBuff);
 }
 
 void world_draw_black_tile(BYTE *pBuff)
@@ -273,14 +299,16 @@ void world_draw_black_tile(BYTE *pBuff)
 
 	for (i = 30, j = 1; i >= 0; i -= 2, j++, dst -= BUFFER_WIDTH + 64) {
 		dst += i;
-		for (k = 0; k < 4 * j; k++)
+		for (k = 0; k < 4 * j; k++) {
 			*dst++ = 0;
+		}
 		dst += i;
 	}
 	for (i = 2, j = 15; i != 32; i += 2, j--, dst -= BUFFER_WIDTH + 64) {
 		dst += i;
-		for (k = 0; k < 4 * j; k++)
+		for (k = 0; k < 4 * j; k++) {
 			*dst++ = 0;
+		}
 		dst += i;
 	}
 }
